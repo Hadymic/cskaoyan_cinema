8c8f49f0d0becbc34e3ec640155bc8f6a155d9bb
jszza <myangj@163.com>
2019-10-14 21:27:06 +0800

    author:jszza msg: create mapper and dao

---
+++ guns-film/src/main/java/com/cskaoyan/cinema/rest/common/persistence/dao/ActorTMapper.java
@@ -0,0 +1,16 @@
+package com.cskaoyan.cinema.rest.common.persistence.dao;
+
+import com.cskaoyan.cinema.rest.common.persistence.model.ActorT;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 演员表 Mapper 接口
+ * </p>
+ *
+ * @author jszza
+ * @since 2019-10-14
+ */
+public interface ActorTMapper extends BaseMapper<ActorT> {
+
+}
---
+++ guns-film/src/main/java/com/cskaoyan/cinema/rest/common/persistence/dao/FilmInfoTMapper.java
@@ -0,0 +1,16 @@
+package com.cskaoyan.cinema.rest.common.persistence.dao;
+
+import com.cskaoyan.cinema.rest.common.persistence.model.FilmInfoT;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 影片主表 Mapper 接口
+ * </p>
+ *
+ * @author jszza
+ * @since 2019-10-14
+ */
+public interface FilmInfoTMapper extends BaseMapper<FilmInfoT> {
+
+}
