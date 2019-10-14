76c4792094b25556136298c23e2b4fdaddeb29e2
jszza <myangj@163.com>
2019-10-14 22:35:57 +0800

    author:jszza msg: controller and service

--- guns-film/src/main/java/com/cskaoyan/cinema/rest/modular/impl/FilmServiceImpl.java
+++ guns-film/src/main/java/com/cskaoyan/cinema/rest/modular/impl/FilmServiceImpl.java
@@ -2,8 +2,12 @@ package com.cskaoyan.cinema.rest.modular.impl;
 
 import com.cskaoyan.cinema.rest.common.persistence.dao.*;
 import com.cskaoyan.cinema.service.FilmService;
+import org.apache.dubbo.config.annotation.Service;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
+@Service(interfaceClass = FilmService.class)
 public class FilmServiceImpl implements FilmService {
     @Autowired
     private ActorTMapper actorTMapper;
@@ -21,4 +25,6 @@ public class FilmServiceImpl implements FilmService {
     private SourceDictTMapper sourceDictTMapper;
     @Autowired
     private YearDictTMapper yearDictTMapper;
+
+
 }
--- guns-gateway/src/main/java/com/cskaoyan/cinema/rest/common/persistence/controller/FilmController.java
+++ guns-gateway/src/main/java/com/cskaoyan/cinema/rest/common/persistence/controller/FilmController.java
@@ -1,4 +1,14 @@
 package com.cskaoyan.cinema.rest.common.persistence.controller;
 
+import com.cskaoyan.cinema.service.FilmService;
+import org.apache.dubbo.config.annotation.Reference;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("film")
 public class FilmController {
+    @Reference(interfaceClass = FilmService.class)
+    private FilmService filmService;
+
 }
