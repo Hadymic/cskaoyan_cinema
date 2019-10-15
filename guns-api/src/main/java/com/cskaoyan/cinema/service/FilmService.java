dbb4534dafd55af4be32ef1978d8a3b048a33849
Zeng-jz <1017055889@qq.com>
2019-10-15 17:44:41 +0800

    author: Zeng-jz msg:影片条件列表查询接口、影片接口查询

--- guns-api/src/main/java/com/cskaoyan/cinema/service/FilmService.java
+++ guns-api/src/main/java/com/cskaoyan/cinema/service/FilmService.java
@@ -1,4 +1,9 @@
 package com.cskaoyan.cinema.service;
 
+import com.cskaoyan.cinema.vo.film.ConditionNoVO;
+
 public interface FilmService {
+    Object selectConfitionList(ConditionNoVO conditionNoVO);
+
+    Object selectFilms(ConditionNoVO conditionNoVO, int showType, int sortId, int pageSize, int offset);
 }
--- guns-gateway/src/main/java/com/cskaoyan/cinema/rest/common/persistence/controller/FilmController.java
+++ guns-gateway/src/main/java/com/cskaoyan/cinema/rest/common/persistence/controller/FilmController.java
@@ -1,7 +1,12 @@
 package com.cskaoyan.cinema.rest.common.persistence.controller;
 
+import com.baomidou.mybatisplus.plugins.Page;
 import com.cskaoyan.cinema.service.FilmService;
+import com.cskaoyan.cinema.vo.BaseRespVo;
+import com.cskaoyan.cinema.vo.film.ConditionNoVO;
+import com.cskaoyan.cinema.vo.film.ConditionVO;
 import org.apache.dubbo.config.annotation.Reference;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +16,42 @@ public class FilmController {
     @Reference(interfaceClass = FilmService.class)
     private FilmService filmService;
 
+    /**
+     * Zeng-jz
+     * @param conditionNoVO
+     * @return
+     */
+    @RequestMapping("getConditionList")
+    public BaseRespVo getConditionList(ConditionNoVO conditionNoVO){
+        Object conditionVo = filmService.selectConfitionList(conditionNoVO);
+        if (conditionVo != null) {
+            BaseRespVo<Object> condition = new BaseRespVo<>();
+            condition.setStatus(0);
+            condition.setData(conditionVo);
+            condition.setMsg(null);
+            return condition;
+        }else {
+            BaseRespVo<Object> condition = new BaseRespVo<>();
+            condition.setStatus(1);
+            condition.setData(null);
+            condition.setMsg("查询失败，无条件可加载");
+            return condition;
+        }
+    }
+
+    /**
+     * Zeng-jz
+     * @param conditionNoVO
+     * @param showType
+     * @param sortId
+     * @param pageSize
+     * @param offset
+     * @return
+     */
+    @RequestMapping("getFilms")
+    public Object getFilms(@RequestBody ConditionNoVO conditionNoVO, int showType,
+                                int sortId, int pageSize, int offset){
+        Object films = filmService.selectFilms(conditionNoVO, showType, sortId, pageSize, offset);
+        return films;
+    }
 }
