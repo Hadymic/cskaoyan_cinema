160eecb8ce92b4edf2fc0466ad9a15b920d2543c
stark_h <514841961@qq.com>
2019-10-15 09:19:22 +0800

    auhtor:hjl msg pull

--- guns-gateway/src/main/java/com/cskaoyan/cinema/rest/common/persistence/controller/UserController.java
+++ guns-gateway/src/main/java/com/cskaoyan/cinema/rest/common/persistence/controller/UserController.java
@@ -4,10 +4,7 @@ import com.cskaoyan.cinema.service.UserService;
 import com.cskaoyan.cinema.vo.BaseRespVo;
 import com.cskaoyan.cinema.vo.user.UserRegisterVo;
 import org.apache.dubbo.config.annotation.Reference;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("user")
@@ -17,6 +14,22 @@ public class UserController {
 
     @PostMapping("register")
     public BaseRespVo register(@RequestBody UserRegisterVo vo) {
-        return new BaseRespVo(1, null, null);
+        return new BaseRespVo();
+    }
+
+    @GetMapping("logout")
+    public BaseRespVo logout() {//Authorization
+        return new BaseRespVo();
+    }
+
+    @GetMapping("getUserInfo")
+    public BaseRespVo getUserInfo() {
+        BaseRespVo baseRespVo = userService.selectUserInfo();
+        return baseRespVo;
+    }
+
+    @PostMapping("updateUserInfo")
+    public BaseRespVo updateUserInfo(){
+
     }
 }
