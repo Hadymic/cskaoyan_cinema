950e846c716cf3f10785f61e0270d0aae8b821f8
jszza <myangj@163.com>
2019-10-16 19:34:27 +0800

    author: jszza msg: for pull

--- guns-api/src/main/java/com/cskaoyan/cinema/service/OrderService.java
+++ guns-api/src/main/java/com/cskaoyan/cinema/service/OrderService.java
@@ -1,4 +1,11 @@
 package com.cskaoyan.cinema.service;
 
 public interface OrderService {
+
+    /**
+     * 根据FieldId获取所有已经销售的座位编号
+     * @param fieldId
+     * @return
+     */
+   // String getSoldSeatsByFieldId(Integer fieldId);
 }
--- guns-order/src/main/java/com/cskaoyan/cinema/rest/common/persistence/service/impl/OrderServiceImpl.java
+++ guns-order/src/main/java/com/cskaoyan/cinema/rest/common/persistence/service/impl/OrderServiceImpl.java
@@ -3,9 +3,22 @@ package com.cskaoyan.cinema.rest.common.persistence.service.impl;
 
 import com.cskaoyan.cinema.service.OrderService;
 import org.apache.dubbo.config.annotation.Service;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 @Service(interfaceClass = OrderService.class)
 public class OrderServiceImpl implements OrderService {
+    /*@Autowired
+    OrderMap orderMap;
+
+
+    @Override
+    public String getSoldSeatsByFieldId(Integer fieldId) {
+        return orderMap.;
+    }*/
+
+    private String getSoldSeats(List ){
+
+    }
 }
