88832f1c1d5c360346e30d634b8eea5f2d71844d
jszza <myangj@163.com>
2019-10-16 15:19:01 +0800

    author:jszza msg:for pull

--- guns-gateway/src/main/java/com/cskaoyan/cinema/rest/common/persistence/controller/FilmController.java
+++ guns-gateway/src/main/java/com/cskaoyan/cinema/rest/common/persistence/controller/FilmController.java
@@ -1,6 +1,8 @@
 package com.cskaoyan.cinema.rest.common.persistence.controller;
 
 
+import com.cskaoyan.cinema.core.exception.GunsException;
+import com.cskaoyan.cinema.rest.common.exception.FilmExceptionEnum;
 import com.cskaoyan.cinema.service.FilmService;
 import com.cskaoyan.cinema.vo.film.FilmVO;
 import org.apache.dubbo.config.annotation.Reference;
@@ -21,7 +23,7 @@ public class FilmController {
     public FilmVO getIndex() {
         Object indexVO = filmService.selectFilms4Index();
         if (indexVO == null) {
-            return new FilmVO(1, "查询失败，无影片可加载");
+            throw new GunsException(FilmExceptionEnum.FILM_NOT_FOUND);
         }
         return new FilmVO<>(0, indexVO, null);
     }
@@ -29,11 +31,11 @@ public class FilmController {
     @RequestMapping("films/{name}")
     public FilmVO getFilmInfo(@PathVariable String name, Integer searchType) {
         if (searchType != 0 && searchType != 1) {
-            return new FilmVO(400, "系统出现异常，输入不合法");
+            throw new GunsException(FilmExceptionEnum.VAR_REQUEST_NULL);
         }
         Object filmInfoVO = filmService.selectFilmInfo(name, searchType);
         if (filmInfoVO == null) {
-            return new FilmVO(1, "查询失败，无影片可加载");
+            throw new GunsException(FilmExceptionEnum.FILM_NOT_FOUND);
         }
         return new FilmVO<>(0, filmInfoVO, null);
     }
