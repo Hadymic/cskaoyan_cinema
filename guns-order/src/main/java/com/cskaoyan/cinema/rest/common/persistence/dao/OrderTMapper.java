0032f325ecbb93e992f71609321dbbc28a8083fe
Hadymic <hadymic@gmail.com>
2019-10-16 17:47:21 +0800

    hadymic add alipay service

---
+++ guns-order/src/main/java/com/cskaoyan/cinema/rest/common/persistence/dao/OrderTMapper.java
@@ -0,0 +1,16 @@
+package com.cskaoyan.cinema.rest.common.persistence.dao;
+
+import com.cskaoyan.cinema.rest.common.persistence.model.OrderT;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 订单信息表 Mapper 接口
+ * </p>
+ *
+ * @author hadymic
+ * @since 2019-10-16
+ */
+public interface OrderTMapper extends BaseMapper<OrderT> {
+
+}
--- guns-order/src/main/java/com/cskaoyan/cinema/rest/common/persistence/service/impl/OrderServiceImpl.java
+++ guns-order/src/main/java/com/cskaoyan/cinema/rest/common/persistence/service/impl/OrderServiceImpl.java
@@ -1,11 +1,15 @@
 package com.cskaoyan.cinema.rest.common.persistence.service.impl;
 
 
+import com.cskaoyan.cinema.rest.common.persistence.dao.OrderTMapper;
 import com.cskaoyan.cinema.service.OrderService;
 import org.apache.dubbo.config.annotation.Service;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 @Service(interfaceClass = OrderService.class)
 public class OrderServiceImpl implements OrderService {
+    @Autowired
+    private OrderTMapper orderTMapper;
 }
